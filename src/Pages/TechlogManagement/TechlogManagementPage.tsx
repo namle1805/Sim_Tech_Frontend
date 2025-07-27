16c212ab081e1df067b08dc71c92c7625554b46f
Loc Ngo-Huynh-Tan <locngohuynhtan@gmail.com>
2025-07-27 14:30:50 +0700

    feat: setup default pages with routing and implement sidebar navigation

---
+++ src/Pages/TechlogManagement/TechlogManagementPage.tsx
@@ -0,0 +1,5 @@
+const TechlogManagementPage = () => {
+  return <>Techlog Management Page</>;
+};
+
+export default TechlogManagementPage;
--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,10 +1,41 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
 import Layout from "../Pages/Layout/Layout";
+import ItemPage from "../Pages/Item/ItemPage";
+import TemplatePage from "../Pages/Template/TemplatePage";
+import TechlogManagementPage from "../Pages/TechlogManagement/TechlogManagementPage";
+import UserPage from "../Pages/User/UserPage";
+import WarehouseManagementPage from "../Pages/WarehouseManagement/WarehouseManagementPage";
+import ReportManagementPage from "../Pages/ReportManagement/ReportManagementPage";
 
-export const router = createBrowserRouter ([
-    {
-        path:'/',
-        element: <Layout/>
-    }
-])
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <ItemPage />,
+      },
+      {
+        path: "template",
+        element: <TemplatePage />,
+      },
+      {
+        path: "techlog-management",
+        element: <TechlogManagementPage />,
+      },
+      {
+        path: "user",
+        element: <UserPage />,
+      },
+      {
+        path: "warehouse-management",
+        element: <WarehouseManagementPage />,
+      },
+      {
+        path: "report-management",
+        element: <ReportManagementPage />,
+      },
+    ],
+  },
+]);
